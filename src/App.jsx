a03b43f6dc89f01b0c762dff5e413029bc3de208
Trương Quang Tường <21522777@gm.uit.edu.vn>
2025-06-07 15:52:47 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-2xl font-bold underline text-blue-600">
+        Hello, Tailwind CSS with React and Vite!
+      </h1>
+    </>
+  );
+}
+
+export default App;
