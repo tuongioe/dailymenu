12d49d5ccec38f7caabf10c892b6531236e6ac26
Trương Quang Tường <21522777@gm.uit.edu.vn>
2025-06-09 16:14:06 +0700

    Update HomePage

--- src/components/HomePageBody.jsx
+++ src/components/HomePageBody.jsx
@@ -3,58 +3,22 @@ import grill from "../assets/img/grill.png";
 import noodle from "../assets/img/noodle.png";
 import salad from "../assets/img/salad.png";
 import meat from "../assets/img/meat.png";
+import onion from "../assets/img/onion.png";
 
 function HomePageBody() {
   return (
     <div className="HomePageBody__Wrapper">
-      <img
-        src={grill}
-        alt=""
-        style={{
-          maxWidth: "10rem",
-          marginLeft: "15rem",
-          marginTop: "3rem",
-          transform: "rotate(5deg)",
-          filter: "drop-shadow(20px 20px 40px rgba(0,0,0,0.3))",
-        }}
-      />
-      <img
-        src={noodle}
-        alt=""
-        style={{
-          maxWidth: "20rem",
-          marginLeft: "15rem",
-          marginTop: "3rem",
-          transform: "rotate(10deg)",
-          filter: "drop-shadow(30px 30px 60px rgba(0,0,0,0.3))",
-        }}
-      />
-      <img
-        src={salad}
-        alt=""
-        style={{
-          maxWidth: "25rem",
-          marginLeft: "6rem",
-          marginTop: "3rem",
-          transform: "rotate(-20deg)",
-          filter: "drop-shadow(30px 30px 60px rgba(0,0,0,0.3))",
-        }}
-      />
-      <img
-        src={meat}
-        alt=""
-        style={{
-          maxWidth: "35rem",
-          width: "35em",
-          marginLeft: "48rem",
-          marginTop: "5rem",
-          filter: "drop-shadow(30px 30px 60px rgba(0,0,0,0.3))",
-        }}
-      />
+      <img src={grill} alt="" className="HomePageBody__Image--Grill" />
+      <img src={noodle} alt="" className="HomePageBody__Image--Noodle" />
+      <img src={salad} alt="" className="HomePageBody__Image--Salad" />
+      <img src={meat} alt="" className="HomePageBody__Image--Meat" />
+      <img src={onion} alt="" className="HomePageBody__Image--Onion" />
       <div className="HomePageBody__Content">
         <div className="HomePageBody__Cap">Daily Menu</div>
-        <button className="HomePageBody__Button--1">About us</button>
-        <button className="HomePageBody__Button--2">Discover more</button>
+        <div className="HomePageBody__Button--Cover"></div>
+        <button className="HomePageBody__Button">About us</button>
+        <button className="HomePageBody__Button">Discover more</button>
+        <p></p>
       </div>
     </div>
   );
